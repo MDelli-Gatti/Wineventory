cc8f5eb2fa247ccc6bd867778ea90045df34f677
michaeldelli-gatti <michaelddg@gmail.com>
2017-08-06 14:19:31 -0400

    Set up HTML and h2 server

--- src/main/java/com/delligatti/Wineventory/controllers/WineventoryController.java
+++ src/main/java/com/delligatti/Wineventory/controllers/WineventoryController.java
@@ -1,13 +1,14 @@
 package com.delligatti.Wineventory.controllers;
 
+import org.h2.tools.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import com.delligatti.Wineventory.entities.User;
 import com.delligatti.Wineventory.entities.Item;
 import com.delligatti.Wineventory.services.UserRepository;
 import com.delligatti.Wineventory.services.ItemRepository;
-
 import javax.annotation.PostConstruct;
+import java.sql.SQLException;
 
 @RestController
 public class WineventoryController {
@@ -18,7 +19,7 @@ public class WineventoryController {
     ItemRepository items;
 
     @PostConstruct
-    public void init(){
-
+    public void init() throws SQLException {
+        Server.createWebServer("-webPort", "1337").start();
     }
 }
